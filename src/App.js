e1608ed33a864d6d38423509dbdc57e1965929bd
raelulu <raelulu23@gmail.com>
2023-02-14 18:59:06 +0900

    feat: crew구현중

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Main from './pages/Main.jsx';
 import Challenge from './pages/Challenge.jsx';
-import Mate from './pages/Mate.jsx';
+import Crew from './pages/Crew.jsx';
 import Recommendation from './pages/Recommendation.jsx';
 import Mypage from './pages/Mypage.jsx';
 
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/challenge" element={<Challenge />} />
-        <Route path="/mate" element={<Mate />} />
+        <Route path="/crew" element={<Crew />} />
         <Route path="/recommendation" element={<Recommendation />} />
         <Route path="/mypage" element={<Mypage />} />
       </Routes>
---
+++ src/components/crew/CrewBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Modal from './Modal';
+
+export default function CrewBox() {
+  return (
+    <Modal>
+      <div>
+        <div>CrewBox</div>
+      </div>
+    </Modal>
+  );
+}
