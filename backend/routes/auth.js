f40de44b3bee1c5a84a66ea14cc36983cbad6d2e
DESKTOP-1343178\96tmd <96tmdqh@naver.com>
2023-02-15 17:15:45 +0900

    feat: socket&Login

---
+++ backend/routes/auth.js
@@ -0,0 +1,67 @@
+const User = require('../models/User')
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const {tokenCheck} = require('../middleware/tokenCheck')
+
+
+router.post('/login', async (req, res, next) => {
+  const {id, pw} = req.body
+  console.log(req.body)
+  try {
+    const userInfo = await User.findOne({
+      where: {id}
+    })
+    if(!userInfo) return res.status(403).send('유저 정보가 없습니다.')
+    if(userInfo.pw != pw) return res.status(403).send('비밀번호가 일치 하지 않습니다.') 
+
+    //access 토큰 발급
+    //3번쨰 인자는 발행자, 유효기간 등을 담을 수 있다.
+    // const accessToken = jwt.sign({
+    //   id: userInfo.id,
+    //   name: userInfo.name,
+    // },process.env.REFRESH_SECRET,{
+    //   expiresIn: '24h',//1분
+    //   issuer : 'we'
+    // })
+
+
+    const accessToken = jwt.sign({
+      type: 'JWT',
+      id: userInfo.id,
+      nickname: userInfo.nickname,
+    }, process.env.ACCESS_SECRET, {
+      expiresIn: '15m', // 만료시간 15분
+      issuer: 'we',
+    });
+  
+
+  
+    // token 전송
+    res.cookie('accessToken',accessToken, {
+      secure: false, //https, http차이 명시
+      httpOnly : true
+    })
+  
+
+
+    res.status(200).json({
+      message: '로그인 성공!',
+      data: {
+        id: userInfo.id,
+        nickName: userInfo.nickName
+      }
+    })
+    
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+})
+
+router.post('/userinfo', tokenCheck, async (req, res, next) => {
+  console.log(req.decoded)
+  res.send('로그인 성공')
+}) 
+
+module.exports = router;
---
+++ src/TEST/Chat.jsx
@@ -0,0 +1,125 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./chat.css";
+import { io } from 'socket.io-client';
+
+
+
+
+export default function Chat({setGo, currentRoom, socket }) {
+  console.log('ChatComponent')
+  console.log(currentRoom)
+  const [socketId, setSocketId] = useState(socket.id);
+  const [chatting, setChatting] = useState([]);
+  const inputValue = useRef(null);
+  const [nickName, setnick] = useState(`user${Math.round(Math.random()*10)}`);
+  const room = useRef();
+  
+
+  useEffect(() => {
+    console.log('notice useEffet')
+    socket.emit("notice", { nickName, currentRoom });
+    socket.on("notice", (data) => {
+      console.log(data.msg)
+      console.log(data.msg.split('님')[0])
+      if(data.msg.split('님')[0] != 'undefined'){
+        data["nickName"] = nickName
+        setChatting((chatting) => [...chatting, data]);
+      }
+      //chat페이지에서 새로고침시에 disconnect가 발생하는데 화면에 그리지 않기 위해 조건사용
+    });
+    socket.emit("join", { currentRoom : currentRoom.id });
+    //room을 선택해 입장을 누르면 컴포넌트가 마운트 되면서 방에 입장
+    return () => {
+      socket.off("notice");
+    };
+  }, [])
+  //소켓이 변한다는건 다른 유저가 들어왔다는 거다 그래서 변할 때마다 공지를 해준다.
+
+
+  
+  
+  
+  useEffect(() => {
+    console.log('Msg useEffet')
+    socket.on("newMsg", (data) => {
+      if (socketId == data.from) {
+        data["liClassName"] = "myMessage";
+      } else {
+        data["liClassName"] = "yourMessage";
+        data["divClassName"] = "yourProfile";
+      }
+      setChatting((chat) => [...chat, data]);
+      room.current.scrollTop = room.current.scrollHeight;
+
+    });
+    return () => {
+      socket.off("newMsg");
+    };
+  },[socketId]); 
+  //socketId를 안 넣으면 맨 처음 빈 소켓아이디여서 상대글로 인식한다.
+
+
+ 
+
+  
+  
+
+  const onClick = () => {
+    socket.emit("sendMsg", { msg: inputValue.current.value, nickName, currentRoom }); //게시물 id도 같이 보내야한다.
+    inputValue.current.value = "";
+  };
+  // const SelectOnChange = (e) => setTo(e.target.value);
+  return (
+    <div className="relative">
+        <div className="roomTitle">{currentRoom.title}</div>
+      <div ref={room} className="room">
+        <ul>
+          {chatting.map((data, i , chat) => {
+            return (
+              data.type == 'notice' ? 
+              <p key={i}>{data.msg}</p>
+              :
+              data.from == socketId ? 
+              <li className='myMessage' key={i}>
+                 <p>{data.msg}</p>
+              </li>
+              :
+              chat[i-1]?.from == data.from ? 
+              <li className="yourMessage" key={i}>
+                <p>{data.msg}</p>
+              </li> :
+              <li className="yourMessage" key={i}>
+                <div className="yourProfile">
+                  <img
+                    src="https://t1.daumcdn.net/cfile/tistory/2513B53E55DB206927"
+                    width="40"
+                    height="40"
+                    alt=""
+                  />
+                  <h3>{data.nickName}</h3>
+                </div>
+                <p>{data.msg}</p>
+              </li>
+            );
+          })}
+        </ul>
+        <form action="">
+          <input
+            ref={inputValue}
+            type="text"
+            id="msg_box"
+            // onkeyDown="enter()"
+          />
+          <button type="button" onClick={() => onClick()}>
+            입력
+          </button>
+        </form>
+      </div>
+      <button className="exit" onClick={() => {
+        console.log(1)
+        setGo((state) => !state)
+        socket.emit('roomOut')
+      }}>나가기</button>
+    </div>
+  );
+}
