922c098beccc77a81b021a04c4891db2530c0d29
DESKTOP-1343178\96tmd <96tmdqh@naver.com>
2023-02-18 16:29:58 +0900

    socket2

--- src/TEST/ChatPage.jsx
+++ src/TEST/ChatPage.jsx
@@ -3,18 +3,29 @@ import Chat from './Chat';
 import { io } from 'socket.io-client';
 import axios from 'axios';
 
-let socket = io.connect('http://localhost:8000');
 
 export default function ChatPage() {
-  console.log(socket);
-  const [room, setRomm] = useState([
+  console.log('Chatpage, 크루정보불러와서 2번 렌더링일어남')
+  let socket = io.connect('http://localhost:8000');
+  const [nickName, setnick] = useState(`user${Math.round(Math.random() * 2)}`);
+  const [id, setId] = useState(nickName);
+  const [user, setUser] = useState({
+    id: id,
+    nickName: nickName
+  })
+  const [crew, setCrew] = useState([
     { title: '용산역 일 등 러닝 크루!!', id: 1, max: 4, member: [] },
     { id: 2, max: 3 },
     { id: 3, max: 5 },
   ]);
   const [display, setDisplay] = useState(true);
   const [currentCrew, currentCrewSet] = useState('');
-  console.log(currentCrew);
+  console.log(user)
+  console.log(currentCrew)
+
+
+
+
 
   useEffect(() => {
     AllmatePostLoad();
@@ -26,30 +37,47 @@ export default function ChatPage() {
       url: `http://localhost:8000/mate`,
     });
     console.log(data);
-    setRomm(data.data);
+    setCrew(data.data);
   }
 
   async function joinCrew() {
-    if (currentCrew.users.find((e) => e.nickName == 'ㅁㄴㅇ')) {
+    if (currentCrew.users.find((e) => e.nickName == user.nickName)) {
       //데미더이터
       //내 유저 아이디와 같은게 있다면
       //입장
       setDisplay((state) => !state);
+      alert('웰컴')
     } else if (currentCrew.max <= currentCrew.users.length) {
       alert('인원이 초과되었습니다.');
     } else {
       const data = await axios({
         method: 'post',
-        url: `http://localhost:8000/mate/addcrew`,
+        url: 'http://localhost:8000/mate/addcrew',
         data: {
-          nickName: 'ㅁㄴㅇ', //더미데이터
-          currentCrew: currentCrew.id,
+          nickName: user.nickName, //더미데이터
+          id: currentCrew.id,
         },
       });
       console.log(data);
       setDisplay((state) => !state);
     }
   }
+
+  async function outCrew() {
+      alert('정말로 탈퇴하시겠습니까?');
+
+      const data = await axios({
+        method: 'delete',
+        url: 'http://localhost:8000/mate/outcrew',
+        data: {
+          nickName: user.nickName, //더미데이터
+          id: currentCrew.id,
+        },
+      });
+      console.log(data);
+      // setDisplay((state) => !state);
+    }
+  
 
   function selectCrew(e) {
     currentCrewSet(e);
@@ -61,18 +89,22 @@ export default function ChatPage() {
 
   return (
     <div>
-      {room.map((e, i) => (
+      {crew.map((e, i) => (
         <button key={i} onClick={() => selectCrew(e)}>
           게시물
         </button>
       ))}
       {display ? (
+        <>
         <button onClick={() => joinCrew()}>입장</button>
+        <button onClick={() => outCrew()}>탈퇴</button>
+        </>
       ) : (
         <Chat
           setDisplay={setDisplay}
           currentCrew={currentCrew}
           socket={socket}
+          user={user}
         />
       )}
     </div>
